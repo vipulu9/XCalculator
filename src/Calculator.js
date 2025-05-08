4a30e758a624839d0c25c7d197ca01fb37c60523
vipulu9 <vipuluniyal9@gmail.com>
2025-05-08 20:26:30 +0530

    XCalculator

---
+++ src/Calculator.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import './Calculator.css';
+
+const Calculator = () => {
+  const [input, setInput] = useState('');
+  const [result, setResult] = useState('');
+
+  const handleClick = (value) => {
+    if (value === '=') {
+      try {
+        if (input.trim() && !/[\+\-\*\/]$/.test(input)) {
+          setResult(eval(input));
+        } else {
+          setResult('Error');
+        }
+      } catch {
+        setResult('Error');
+      }
+    } else if (value === 'C') {
+      setInput('');
+      setResult('');
+    } else {
+      setInput(input + value);
+    }
+  };
+
+  return (
+    <div className="calculator">
+      <h1>React Calculator</h1>
+      <input type="text" value={input} readOnly />
+      <div className="result">{result}</div>
+      <div className="buttons">
+        {['7', '8', '9', '+', '4', '5', '6', '-', '1', '2', '3', '*', 'C', '0', '=', '/'].map((btn) => (
+          <button key={btn} onClick={() => handleClick(btn)}>
+            {btn}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Calculator;
